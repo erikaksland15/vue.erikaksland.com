d4edb3963518cce18ea95c689544c3ce1f4e6520
Erik Aksland <erikaksland15@gmail.com>
2020-05-08 13:34:04 -0400

    add body page

---
+++ src/components/webpages/PomodoroTimerBody.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pomodoro-timer-body">
+    <h3>This is a start, of my page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PomodoroTimerBody"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
--- src/views/PomodoroTimer.vue
+++ src/views/PomodoroTimer.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="pomodoro-timer">
     <h1>This is my Pomodoro Timer</h1>
+    <PomodoroTimerBody />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import PomodoroTimerBody from "@/components/webpages/PomodoroTimerBody.vue";
+
+export default {
+  name: "PomodoroTimer",
+  components: {
+    PomodoroTimerBody
+  }
+};
+</script>
