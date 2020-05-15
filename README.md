b669ea4db3c31124bd17a0e516338a4b86c633cf
Erik Aksland <erikaksland15@gmail.com>
2020-05-15 08:42:43 -0400

    add to readme

--- README.md
+++ README.md
@@ -1,15 +1,42 @@
 # vue.erikaksland.com
 
+## Pre Project setup
+This pomodoro timer runs using vue.js, php, and mysql.
+
+Things to install first:
+XAMPP: https://www.apachefriends.org/index.html
+  ^this should install php and mysql onto your local machine to run this app
+node.js (LTS): https://nodejs.org/en/
+  ^needed to run serve and vue aspects of this app
+*MySQL Workbench: https://dev.mysql.com/downloads/workbench/
+  ^may be needed depending on local configuration to get sql to run from scripts
+
+
+
 ## Project setup
+### Installs node_modules needed to run locally
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Compiles and hot-reloads for development - readys and loads the site at http://localhost:8080/ (might vary based on your local development server)
 ```
 npm run serve
 ```
 
+
+
+
+
+
+
+
+
+
+
+
+
+### Other
 ### Compiles and minifies for production
 ```
 npm run build
---
+++ demo_create_db.js
@@ -0,0 +1,44 @@
+const dbName = "pomodoro_timer_db";
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(error)
+{
+  if (error)
+  {
+    throw error;
+  }
+  console.log("Connected!");
+
+  var sqlCreateDatabase = "CREATE DATABASE " + dbName;
+  con.query(sqlCreateDatabase, function (error, result)
+  {
+    if (error)
+    {
+      throw error;
+    }
+    console.log("Database created");
+  });
+
+  var sqlCreateTable = "CREATE TABLE " + dbName + ".timer_data " +
+                       "(id INT(11) AUTO_INCREMENT PRIMARY KEY, " +
+                        "task_description VARCHAR(255), " +
+                        "task_completed int(1), " +
+                        "created_at VARCHAR(255), " +
+                        "end_time VARCHAR(255))";
+  con.query(sqlCreateTable, function (error, result)
+  {
+    if (error)
+    {
+      throw error;
+    }
+    console.log("Table created");
+    console.log("Hooray! Much Success");
+    process.exit();
+  });
+});
